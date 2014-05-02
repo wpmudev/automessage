5bf3a688131a20c4bef43043fad3f793319bda75
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-05-02 18:31:19 +0000

    Fix WPMUDEV author names
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/automessage@14148 f53fe46e-8c02-459b-9fdd-93e75771b278

--- automessage.php
+++ automessage.php
@@ -3,7 +3,7 @@
 Plugin Name: Automessage
 Plugin URI: http://premium.wpmudev.org/project/automatic-follow-up-emails-for-new-users
 Description: This plugin allows emails to be scheduled and sent to new users.
-Author: WPMUDEV
+Author: WPMU DEV
 Version: 2.4
 Author URI: http://caffeinatedb.com
 WDP ID: 81
